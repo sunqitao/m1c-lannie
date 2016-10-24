9acbbe9a0a6858bfcaf166ce41ce4f09a907b562
mac <mac@macdeMBP-5.lan>
2016-10-24 10:08:40 +0800

    add request son

--- m1c-lannie-app-maven/src/main/java/cn/m1c/testmodule/person/controller/UserController.java
+++ m1c-lannie-app-maven/src/main/java/cn/m1c/testmodule/person/controller/UserController.java
@@ -5,27 +5,32 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alibaba.fastjson.JSONObject;
 
 import cn.m1c.frame.exception.M1CRuntimeException;
 import cn.m1c.frame.vo.RpcResult;
+import cn.m1c.testmodule.person.model.User;
 import cn.m1c.testmodule.person.service.UserService;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/api2/account")
 public class UserController {
 	@Resource
 	private UserService userService;
 	
 	private static Logger logger = LoggerFactory.getLogger(UserController.class);
 	
-	@SuppressWarnings("unused")
-	@RequestMapping("/login")
-	private void login(HttpServletResponse response,String loginName,String pwd){
+	@RequestMapping(value = "/loginByPhone",method = RequestMethod.POST )
+	private void login(HttpServletResponse response,@RequestBody User  user ){
 		RpcResult rpcResult = RpcResult.success(); 
 		try {
-			String reString = userService.login(loginName, pwd);
+			String reString = userService.login("","");
 			rpcResult.addDatabody("result", reString);
 		} catch (M1CRuntimeException e) {
 			rpcResult = RpcResult.status(e.getStatusCode());
---
+++ m1c-lannie-app-maven/src/main/java/cn/m1c/testmodule/person/model/User.java
@@ -0,0 +1,21 @@
+package cn.m1c.testmodule.person.model;
+
+public class User {
+	private String loginName;
+	private String pwd;
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
+}
